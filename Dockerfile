587a3bdc7a7feec90fd300285c92e31e05272f19
m48483 <71882579+m48483@users.noreply.github.com>
2024-08-19 17:05:05 +0900

    fix

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 #
-FROM python:3.9
+FROM python:3.10
 
 #
 WORKDIR /code
@@ -7,11 +7,17 @@ WORKDIR /code
 #
 COPY ./requirements.txt /code/requirements.txt
 
+RUN pip install --upgrade pip
+
 #
 RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
 
+RUN pip install "fastapi[standard]"
 #
 COPY . /code
 
+ENV profiles="prod"
+
 #
-CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
+#CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000","--proxy-headers"]
+CMD ["fastapi", "run", "main.py", "--port", "8000"]
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from os import environ
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routers.websocket import router as websocket_router
@@ -8,20 +10,14 @@ app = FastAPI()
 origins = [
     "http://localhost",
     "http://192.168.28.135",
-    "http://192.168.0.15:8081",  # React 개발 서버의 기본 포트
-    "http://192.168.0.15:5173",  # Expo 개발 서버의 기본 포트
-    "http://localhost:5173",  # Expo 개발 서버의 기본 포트
+    "http://192.168.0.15:8081",
+    "http://192.168.0.15:5173",
+    "http://localhost:5173",
+    "https://waguwagu.shop",
     # 필요한 경우 다른 출처도 추가 가능
 ]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,  # 허용할 출처 목록
-    allow_credentials=True,
-    allow_methods=["*"],  # 허용할 HTTP 메서드
-    allow_headers=["*"],  # 허용할 HTTP 헤더
-)
-# 웹소켓 라우터 포함
+
 app.include_router(websocket_router)
 
 @app.get("/alarm")
@@ -30,4 +26,15 @@ async def read_root():
 
 if __name__ == '__main__':
     import uvicorn
+    profiles = environ.get("profiles")
+
+    if profiles != "prod":
+        app.add_middleware(
+            CORSMiddleware,
+            allow_origins=origins,  # 허용할 출처 목록
+            allow_credentials=True,
+            allow_methods=["*"],  # 허용할 HTTP 메서드
+            allow_headers=["*"],  # 허용할 HTTP 헤더
+        )
+    # 웹소켓 라우터 포함
     uvicorn.run(app, host="0.0.0.0", port=8000)
